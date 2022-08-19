71a9a3780992cf8bc4803041e7a494b8883afa45
rishbitsbytes <rishrathore1613@gmail.com>
2022-08-19 13:08:03 +0530

    fixed: issue#20  fixed, homepage categories navigation filter states

--- src/pages/ProductListingPage/ProductListingPage.jsx
+++ src/pages/ProductListingPage/ProductListingPage.jsx
@@ -1,14 +1,32 @@
 import "./ProductListingPage.css";
 import { useDocumentTitle } from "../../custom-hooks";
-import { useEffect } from "react";
+import { useFilter } from "../../contexts";
 import { FiltersWrapperSidebar, ProductListingSection } from "../../components";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 const ProductListingPage = () => {
   const [setDocumentTitle] = useDocumentTitle();
+  const { filterDispatch } = useFilter();
+  const location = useLocation();
+  console.log("log", location.state);
 
   useEffect(() => {
     setDocumentTitle("Products || Petkart");
   }, []);
+
+  useEffect(() => {
+    if (location.state) {
+      const { filterType, filterCategory } = location?.state;
+      filterDispatch({
+        type: "CLEAR_ALL_FILTERS",
+      });
+      filterDispatch({
+        type: filterType,
+        payload: filterCategory,
+      });
+    }
+  }, [location?.state]);
 
   return (
     <div>
